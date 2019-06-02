793b823c48d98db903137652ecf89ae52da09514
andrewasterix <andrea@valgraveglia.net>
2019-06-02 09:43:37 +0200

    Update Exercize

--- ClientUploadOnServer/Client_file_upload_server.c
+++ ClientUploadOnServer/Client_file_upload_server.c
@@ -11,24 +11,40 @@
 
 int main(int argc, char *argv[]){
 
-    int sfd =0, n=0, b;
+    int sock =0, n=0, b;
     char rbuff[1024];
     char sendbuffer[100];
 
     struct sockaddr_in serv_addr;
 
+    if (argc != 3) {
+      fprintf(stderr, "Usage %s ip-address port\n", argv[0]);
+      exit(-1);
+    }
+    
     memset(rbuff, '0', sizeof(rbuff));
-    sfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(5000);
-    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(atoi(argv[2]));
 
-    b=connect(sfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    if (b==-1) {
-        perror("Connect");
-        return 1;
-    }
+    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
 
     FILE *fp = fopen("text.txt", "r");
     if(fp == NULL){
@@ -37,7 +53,7 @@ int main(int argc, char *argv[]){
     }
 
     while( (b = fread(sendbuffer, 1, sizeof(sendbuffer), fp))>0 ){
-        send(sfd, sendbuffer, b, 0);
+        send(sock, sendbuffer, b, 0);
     }
 
     fclose(fp);
--- ClientUploadOnServer/Server_file_reciver_client.c
+++ ClientUploadOnServer/Server_file_reciver_client.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/un.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,40 +12,62 @@
 
 int main(int argc, char *argv[]){
 
-    int fd =0, confd = 0,b,tot;
+    int sock =0, confd = 0,b,tot;
     struct sockaddr_in serv_addr;
 
     char buff[1025];
     int num;
 
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    printf("Socket created\n");
+    if (argc != 2) {
+        fprintf(stderr,"Usage: %s port\n", argv[0]);
+        exit(-1);
+    }
 
+    int port= atoi(argv[1]);
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+                fprintf(stderr, "Error in socket\n");
+                exit(-1);
+        }
+    
+    fprintf (stderr, "socket done\n");
+    
     memset(&serv_addr, '0', sizeof(serv_addr));
     memset(buff, '0', sizeof(buff));
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(5000);
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_addr.s_addr=INADDR_ANY;
+    serv_addr.sin_port= htons(port);
 
-    bind(fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    listen(fd, 10);
+    if(bind(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+        fprintf(stderr, "Error in bind\n");
+        exit(-1);
+    }
+    fprintf(stderr, "Bind Done\n");
 
-    confd = accept(fd, (struct sockaddr*)NULL, NULL);
-        if (confd==-1) {
-            perror("Accept");
-        }
-        FILE* fp = fopen( "text_received.txt", "w");
-        tot=0;
-        if(fp != NULL){
-            while( (b = recv(confd, buff, 1024,0))> 0 ) {
+    if(listen(sock, 10) < 0){
+        fprintf(stderr, "Error in listen\n");
+        exit(-1);
+    }
+    fprintf(stderr, "Listen Done\n");
+
+    confd = accept(sock, (struct sockaddr*)NULL, NULL);
+    if (confd==-1) {
+        perror("Accept");
+    }
+        
+    FILE* fp = fopen( "text_received.txt", "w");
+    tot=0;
+    if(fp != NULL){
+        while( (b = recv(confd, buff, 1024,0))> 0 ) {
                 tot+=b;
                 fwrite(buff, 1, b, fp);
-            }
+        }
 
-            printf("Received byte: %d\n",tot);
-            if (b<0)
-               perror("Receiving");
+        printf("Received byte: %d\n",tot);
+        
+        if (b<0)
+            perror("Receiving");
 
             fclose(fp);
         } else {
@@ -52,6 +75,5 @@ int main(int argc, char *argv[]){
         }
         close(confd);
     
-
     return 0;
 }
